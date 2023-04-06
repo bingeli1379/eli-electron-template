082510aaf15a9a056b86e5c5c2b8a24039ece234
Eli <bingeli1379@gmail.com>
2023-04-06 16:40:12 +0800

    env: add proxy setting

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,53 @@
+/* eslint-env node */
+
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    electron([
-      {
-        entry: 'electron/main.js'
-      },
-      {
-        entry: 'electron/preload.js',
-        onstart(options) {
-          options.reload()
-        }
-      }
-    ])
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+const PORT = 9527
+
+export default ({ mode }) => {
+  const env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd())
   }
-})
+  const proxyUrl = env.VITE_APP_PROXY_URL
+
+  return defineConfig({
+    plugins: [
+      vue(),
+      electron([
+        {
+          entry: 'electron/main.js'
+        },
+        {
+          entry: 'electron/preload.js',
+          onstart(options) {
+            options.reload()
+          }
+        }
+      ])
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        [proxyUrl]: {
+          target: env.VITE_APP_API_URL,
+          changeOrigin: true,
+          ws: true,
+          rewrite: path => path.replace(new RegExp(proxyUrl), '')
+        }
+      },
+      port: PORT
+    },
+    preview: {
+      port: PORT
+    }
+  })
+}
